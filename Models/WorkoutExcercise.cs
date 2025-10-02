5a2205ad8d583ea7ef128cc65acfd2ecd9f9bf35
onni82 <onni.bucht@me.com>
2025-10-02 14:58:38 +0200

    Capitalized prop names

--- Models/WorkoutExcercise.cs
+++ Models/WorkoutExcercise.cs
@@ -5,9 +5,9 @@
         public int Id { get; set; }
         
         public int WorkoutLogId { get; set; }
-        public WorkoutLog workoutLog { get; set; }
+        public WorkoutLog WorkoutLog { get; set; }
         public int ExcerciseId { get; set; }
-        public Exercise exercise { get; set; }
+        public Exercise Exercise { get; set; }
 
         public int Sets { get; set; }
         public int Reps { get; set; }
