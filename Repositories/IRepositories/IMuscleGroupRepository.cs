f38eed28f51e3d133c52a9b40d5e4daa726e36ec
Korkken <Korkken@gmail.com>
2025-10-02 12:14:42 +0200

    Added musclegroup repositories

---
+++ Repositories/IRepositories/IMuscleGroupRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace PapisPowerPracticeApi.Repositories.IRepositories;
+using PapisPowerPracticeApi.Models;
+
+public interface IMuscleGroupRepository
+{
+    public interface IMenuRepository
+    {
+        Task<List<MuscleGroup>> GetAllMuscleGroupsAsync();
+        Task<MuscleGroup> GetMuscleGroupByIdAsync(int muscleGroupId);
+        Task<int> AddMuscleGroupAsync(MuscleGroup muscleGroup);
+        Task<bool> UpdateMuscleGroupAsync(MuscleGroup muscleGroup);
+        Task<bool> DeleteMuscleGroupAsync(int muscleGroupId);
+    }
+}
+
