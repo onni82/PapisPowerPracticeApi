37f68a6294a0ccea369b1534e994243016bf4820
onni82 <onni.bucht@me.com>
2025-10-02 15:06:20 +0200

    added list of MuscleGroup

--- Models/Exercise.cs
+++ Models/Exercise.cs
@@ -5,8 +5,9 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public int MuscleGroupId { get; set; }
-        public MuscleGroup MuscleGroup { get; set; }
         public string VideoUrl { get; set; }
+
+        // Flera-till-flera
+        public ICollection<MuscleGroup> MuscleGroups { get; set; } = new List<MuscleGroup>();
     }
 }
