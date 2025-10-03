307c7f5d077ba6397ed5caab9c969ff3c14734ea
Edwman2 <122057797+Edwman2@users.noreply.github.com>
2025-10-03 12:04:00 +0200

    Deleted entites

--- Models/Exercise.cs
+++ Models/Exercise.cs
@@ -5,8 +5,6 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public int MuscleGroupId { get; set; }
-        public MuscleGroup MuscleGroup { get; set; }
         public string VideoUrl { get; set; }
 
         // Flera-till-flera
