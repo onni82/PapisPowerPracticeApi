e87762f6eb65cc29a61bdd171ef9f0500b4d85f5
Edwman2 <122057797+Edwman2@users.noreply.github.com>
2025-10-03 14:39:00 +0200

    Created classes for register and login user, fixed auth controller

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using PapisPowerPracticeApi.DTOs.Auth.Request;
+using System.Collections.ObjectModel;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace PapisPowerPracticeApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager <IdentityUser> _userManager;
+        private readonly IConfiguration _configuration;
+
+        public AuthController(UserManager<IdentityUser> userManager,IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _configuration = configuration;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterUserDTO userDTO)
+        {
+            var user = new IdentityUser
+            {
+                UserName = userDTO.Email,
+                Email = userDTO.Email
+            };
+
+            var identityResult = await _userManager.CreateAsync(user, userDTO.Password);
+
+            if(!identityResult.Succeeded)
+            {
+                return BadRequest(identityResult.Errors);
+            }
+
+            var addToResult = await _userManager.AddToRoleAsync(user, "User");
+            if(!addToResult.Succeeded)
+            {
+                return BadRequest(addToResult.Errors);
+            }
+
+            return Ok(new { user.Email, Role = "User" });
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginUserDTO loginDTO)
+        {
+            var user = await _userManager.FindByEmailAsync(loginDTO.Email);
+            if (user == null || !await _userManager.CheckPasswordAsync(user, loginDTO.Password))
+            {
+                return Unauthorized();
+            }
+
+            var roles = await _userManager.GetRolesAsync(user);
+
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email!)
+            };
+            claims.AddRange(roles.Select(r => new Claim(ClaimTypes.Role, r)));
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:SecretKey"]!));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials = creds,
+                Issuer = _configuration["Jwt:Issuer"],
+                Audience = _configuration["Jwt:Audience"]
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var accessToken = tokenHandler.WriteToken(token);
+
+            return Ok(new { accessToken });
+                
+        }
+
+        
+    }
+}
---
+++ DTOs/Auth/Request/RegisterUserDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PapisPowerPracticeApi.DTOs.Auth.Request
+{
+    public class RegisterUserDTO
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; } = null;
+    }
+}
