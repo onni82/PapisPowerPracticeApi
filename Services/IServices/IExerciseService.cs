4298caca3f702da42de2ea78b28dc20534836535
onni82 <onni.bucht@me.com>
2025-10-03 11:03:54 +0200

    added exercise service

--- Services/IServices/IExerciseService.cs
+++ Services/IServices/IExerciseService.cs
@@ -1,6 +1,13 @@
-﻿namespace PapisPowerPracticeApi.Services.IServices
+﻿using PapisPowerPracticeApi.Models;
+
+namespace PapisPowerPracticeApi.Services.IServices
 {
     public interface IExerciseService
     {
+        Task<IEnumerable<Exercise>> GetAllExercisesAsync();
+        Task<Exercise?> GetExerciseByIdAsync(int id);
+        Task<Exercise> CreateExerciseAsync(Exercise exercise);
+        Task<Exercise?> UpdateExerciseAsync(int id, Exercise exercise);
+        Task<bool> DeleteExerciseAsync(int id);
     }
 }
