9b17305c17be19bcaca51a75a2a809148618ee6a
onni82 <onni.bucht@me.com>
2025-10-08 14:46:16 +0200

    Added SendMessage

---
+++ PapisPowerPracticeApi/Controllers/ChatController.cs
@@ -0,0 +1,30 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PapisPowerPracticeApi.Services;
+
+namespace PapisPowerPracticeApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class ChatController : ControllerBase
+    {
+        private readonly IChatService _chatService;
+
+        public ChatController(IChatService chatService)
+        {
+            _chatService = chatService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SendMessage([FromBody] string message)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var response = await _chatService.GetAiResponseAsync(userId, message);
+
+            return Ok(new { response });
+        }
+    }
+}
