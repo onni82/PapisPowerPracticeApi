f6392788d451aa5925baf6200e45c64c8023f180
onni82 <onni.bucht@me.com>
2025-10-08 14:52:29 +0200

    Added GetChatHistoryAsync

--- PapisPowerPracticeApi/Services/IServices/IChatService.cs
+++ PapisPowerPracticeApi/Services/IServices/IChatService.cs
@@ -1,7 +1,10 @@
-﻿namespace PapisPowerPracticeApi.Services.IServices
+﻿using PapisPowerPracticeApi.Models;
+
+namespace PapisPowerPracticeApi.Services.IServices
 {
     public interface IChatService
     {
         Task<string> GetAiResponseAsync(string userId, string userMessage);
+        Task<IEnumerable<ChatMessage>> GetChatHistoryAsync(string userId, int limit = 20);
     }
 }
