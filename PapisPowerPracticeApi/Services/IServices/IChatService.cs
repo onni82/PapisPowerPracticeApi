61a7156ef8dd95f10de64eac03d9c6dad4dc294d
onni82 <onni.bucht@me.com>
2025-10-09 14:04:22 +0200

    Renamed argument

--- PapisPowerPracticeApi/Services/IServices/IChatService.cs
+++ PapisPowerPracticeApi/Services/IServices/IChatService.cs
@@ -4,7 +4,7 @@ namespace PapisPowerPracticeApi.Services.IServices
 {
     public interface IChatService
     {
-        Task<string> GetAiResponseAsync(string userId, string userMessage);
+        Task<string> GetAiResponseAsync(string userId, string message);
         Task<IEnumerable<ChatMessage>> GetChatHistoryAsync(string userId, int limit = 20);
     }
 }
