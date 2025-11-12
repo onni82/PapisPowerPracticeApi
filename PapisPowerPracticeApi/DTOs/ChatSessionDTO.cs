3a5c3126612f64bcfd469bfe7e17bcb78031b891
onni82 <onni.bucht@me.com>
2025-11-12 11:47:15 +0100

    Added ChatSessionDTO

---
+++ PapisPowerPracticeApi/DTOs/ChatSessionDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace PapisPowerPracticeApi.DTOs
+{
+    public class ChatSessionDTO
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = null!;
+        public DateTime CreatedAt { get; set; }
+    }
+}
