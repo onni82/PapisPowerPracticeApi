4f5012b2159f6abd1891b95adeb71dcf47af2d62
onni82 <onni.bucht@me.com>
2025-11-12 11:24:19 +0100

    Added ChatMessage and ChatSession

---
+++ PapisPowerPracticeApi/Models/ChatSession.cs
@@ -0,0 +1,11 @@
+﻿namespace PapisPowerPracticeApi.Models
+{
+    public class ChatSession
+    {
+        public Guid Id { get; set; }
+        public string UserId { get; set; } = null!;
+        public string Title { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public ICollection<ChatMessage> Messages { get; set; } = new List<ChatMessage>();
+    }
+}
