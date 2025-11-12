208b0d3650e7b0b31e41f8b6948155f0e96527f6
onni82 <onni.bucht@me.com>
2025-11-12 11:44:41 +0100

    Renamed to ChatRequestDTO

--- PapisPowerPracticeApi/DTOs/ChatRequestDTO.cs
+++ PapisPowerPracticeApi/DTOs/ChatRequestDTO.cs
@@ -1,6 +1,6 @@
 namespace PapisPowerPracticeApi.DTOs
 {
-    public class ChatRequest
+    public class ChatRequestDTO
     {
         public Guid? SessionId { get; set; }
         public string Message { get; set; } = null!;
