89403c22b6c985a1e0b930b8fe44ec785c1e9eb6
onni82 <onni.bucht@me.com>
2025-11-18 13:34:09 +0100

    Changed string GetResponseAsync() to ChatMessageDTO SendMessageAsync() and added GetSessionMessageAsync() and GetUserSessionAsync()

--- PapisPowerPracticeApi/Services/IServices/IChatBotService.cs
+++ PapisPowerPracticeApi/Services/IServices/IChatBotService.cs
@@ -1,7 +1,12 @@
+using PapisPowerPracticeApi.DTOs.Chat.Request;
+using PapisPowerPracticeApi.DTOs.Chat.Response;
+
 namespace PapisPowerPracticeApi.Services.IServices
 {
     public interface IChatBotService
     {
-        Task<string> GetResponseAsync(string message);
+        Task<ChatMessageDTO> SendMessageAsync(string userId, ChatRequestDTO request);
+        Task<IEnumerable<ChatMessageDTO>> GetSessionMessagesAsync(Guid sessionId, string userId);
+        Task<IEnumerable<ChatSessionDTO>> GetUserSessionsAsync(string userId);
     }
 }
